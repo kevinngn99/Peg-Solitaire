740bacd013d78842610098f5ec8da2e88fe1f20b
kevinngn99 <kevinnguyen1@ufl.edu>
2020-12-01 00:58:13 -0500

    Tutorial Modifications

--- client/src/Tutorial.js
+++ client/src/Tutorial.js
@@ -7,6 +7,7 @@ import ClickSelect from './ClickSelect';
 import Carousel from 'react-bootstrap/Carousel';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 function Tutorial() {
     const wi = 1920;
@@ -19,6 +20,7 @@ function Tutorial() {
     const [tutorial, setTutorial] = useState(false);
     const [cursor, setCursor] = useState('Select');
     const [sound, setSound] = useState('On');
+    const [modalShow, setModalShow] = useState(false);
 
     function ItemCard(props) {
         return (
@@ -34,6 +36,56 @@ function Tutorial() {
                     <small className="text-muted"> Card {props.item} of 5 </small>
                 </Card.Footer>
             </Card>
+        );
+    }
+
+    function CustomVerticalModal() {
+        return (
+            <Modal size='lg' show={modalShow} onHide={() => setModalShow(false)} centered={true} animation={true}>
+                <Modal.Header closeButton='true' style={{ color: '#212529', fontSize: 20, fontFamily: 'Inter-Bold'}}>
+                    <Modal.Title>
+                        Tutorial
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body style={{backgroundColor: '#01013d'}}>
+                    <Carousel interval={null} style={{ height: 550, margin: 'auto'}}>
+                        <Carousel.Item>
+                            <ItemCard image='./select/1.png' button={false} title='Basic Rules' item='1'
+                                content='
+                                The rules of peg solitaire are simple.
+                                1) You are allowed to jump over pegs horizontally or vertically.
+                                2) But you can only jump over a peg two positions away.
+                                3) The game is won when there is 1 remaining peg in the center of the board.
+                                That&apos;s it!'
+                            />
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <ItemCard image='./select/2.png' button={false} title='Step One' item='2'
+                                content='The next few slides will teach you the controls of the game.
+                                To move a peg over another peg, hover over your desired peg and select it, as seen above.'
+                            />
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <ItemCard image='./select/3.png' button={true} title='Step Two' item='3'
+                                content='Once you have selected your peg, the game will show which moves you can make.
+                                A green circle indicates a possible move, whereas a yellow circle indicates your currently selected peg.'
+                            />
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <ItemCard image='./select/4.png' button={true} title='Step Three' item='4'
+                                content='To move your currently selected peg over the white peg, hover over to the green circle.
+                                Then select it.'
+                            />
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <ItemCard image='./select/5.png' button={true} title='Step Four' item='5'
+                                content='After selecting the green circle, your peg will automatically move to your desired position.
+                                Repeat this action and leave 1 peg remaining in the center of the board to win!'
+                            />
+                        </Carousel.Item>
+                    </Carousel>
+                </Modal.Body>
+            </Modal>
         );
     }
     
@@ -73,52 +125,6 @@ function Tutorial() {
             <App />
         );
     }
-    else if (tutorial) {
-        return (
-            <div className="Tutorial">
-                <header className="Tutorial-header">
-                    <div id="container" style={{ zoom: scale, margin: 'auto'}} >
-                        <Carousel interval={null} style={{ width: 600, height: 550, margin: 'auto'}}>
-                            <Carousel.Item>
-                                <ItemCard image='./select/1.png' button={false} title='Basic Rules' item='1'
-                                    content='
-                                    The rules of peg solitaire are simple.
-                                    1) You are allowed to jump over pegs horizontally or vertically.
-                                    2) But you can only jump over a peg two positions away.
-                                    3) The game is won when there is 1 remaining peg in the center of the board.
-                                    That&apos;s it!'
-                                />
-                            </Carousel.Item>
-                            <Carousel.Item>
-                                <ItemCard image='./select/2.png' button={false} title='Step One' item='2'
-                                    content='The next few slides will teach you the controls of the game.
-                                    To move a peg over another peg, hover over your desired peg and select it, as seen above.'
-                                />
-                            </Carousel.Item>
-                            <Carousel.Item>
-                                <ItemCard image='./select/3.png' button={true} title='Step Two' item='3'
-                                    content='Once you have selected your peg, the game will show which moves you can make.
-                                    A green circle indicates a possible move, whereas a yellow circle indicates your currently selected peg.'
-                                />
-                            </Carousel.Item>
-                            <Carousel.Item>
-                                <ItemCard image='./select/4.png' button={true} title='Step Three' item='4'
-                                    content='To move your currently selected peg over the white peg, hover over to the green circle.
-                                    Then select it.'
-                                />
-                            </Carousel.Item>
-                            <Carousel.Item>
-                                <ItemCard image='./select/5.png' button={true} title='Step Four' item='5'
-                                    content='After selecting the green circle, your peg will automatically move to your desired position.
-                                    Repeat this action and leave 1 peg remaining in the center of the board to win!'
-                                />
-                            </Carousel.Item>
-                        </Carousel>
-                    </div>
-                </header>
-            </div>
-        );
-    }
     else {
         return (
             <div className="Tutorial">
@@ -133,7 +139,7 @@ function Tutorial() {
                                 Play Game
                             </button>
 
-                            <button onClick={onTutorial} style={{ width: 340, height: 130, margin: 20, outline: 'none' }} className='tutorial'>
+                            <button onClick={() => setModalShow(true)} style={{ width: 340, height: 130, margin: 20, outline: 'none' }} className='tutorial'>
                                 Tutorial
                             </button>
 
@@ -145,6 +151,8 @@ function Tutorial() {
                                 Sound: {sound}
                             </button>
                         </custom-div>
+
+                        <CustomVerticalModal />
                     </div>
                 </header>
             </div>
